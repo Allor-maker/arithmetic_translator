3c11cd368bc7e5262d0efb16ba77a15a9ef18353
P_Alex_Tr <aleksandr.smyshlaev@yandex.ru>
2024-11-30 17:37:42 +0300

    вторая версия

--- include/parser/parser.cpp
+++ include/parser/parser.cpp
@@ -7,55 +7,370 @@ std::vector<char> Parser::pars(std::string str)
 		v.push_back(ch);
 	return v;
 }
-std::string Parser::term_analis()
+Term::Term(double value) :type(Type::NUMBER), value_str(std::to_string(value)) {}
+Term::Term(char ch)
 {
-	std::string input;
-	Stack<std::vector, int> S(std::vector<int> {});
+	if (ch == 40)
+	{
+		type = Type::OPEN_BRACK;
+		value_str.push_back(ch);
+	}
+	if (ch == 41)
+	{
+		type = Type::CLOSE_BRACK;
+		value_str.push_back(ch);
+	}
+	if (ch == 43 || ch == 42 || ch == 45 || ch == 47)
+	{
+		type = Type::OPERATOR;
+		value_str.push_back(ch);
+	}
+	if (ch == 61)
+	{
+		type = Type::EQ;
+		value_str.push_back(ch);
+	}
+}
+Term::Term(std::string str):type(Type::VALUE),value_str(str){}
+double Term::to_value(std::string str)
+{
+	return std::stod(str);
+}
+std::string Term::get_value()
+{
+	return value_str;
+}
+Term::Type Term::get_type()
+{
+	return type;
+}
+std::vector<Term> Parser::term_analis(std::string str)
+{
+	std::vector<Term> output {};
+
 	int status = 0;
-	int parenthesis_counter = 0;
-	char ch = _getch();
-	S.push(0);
-	while (ch != 13)
+	std::string str_value;
+	for (char ch : str)
 	{
 		switch (status)
 		{
 		case 0:
-			if ((ch >= 48 && ch <= 57))
+			if (ch == 40)
+			{
+				status = 7;
+				output.push_back(Term(ch));
+				break;
+			}
+			if (ch >= 48 && ch <= 57)
 			{
 				status = 1;
-				Interface::print_ch(ch);
-				input.push_back(ch);
-				S.push(1);
+				str_value.push_back(ch);
+				break;
+			}
+			if (ch >= 97 && ch <= 122 || ch >= 65 || ch <= 90)
+			{
+				status = 8;
+				str_value.push_back(ch);
+				break;
+			}
+		case 1:
+			if (ch >= 48 && ch <= 57)
+			{
+				status = 1;
+				str_value.push_back(ch);
+				break;
+			}
+			if (ch == 46)
+			{
+				status = 6;
+				str_value.push_back(ch);
+				break;
+			}
+			if (ch == 43 || ch == 42 || ch == 45 || ch == 47)
+			{
+				status = 2;
+				output.push_back(Term(Term::to_value(str_value)));
+				output.push_back(Term(ch));
+				str_value.clear();
+				break;
+			}
+		case 2:
+			if (ch == 40)
+			{
+				status = 0;
+				output.push_back(Term(ch));
+				break;
+			}
+			if (ch >= 48 && ch <= 57)
+			{
+				status = 3;
+				str_value.push_back(ch);
+				break;
+			}
+			if (ch >= 97 && ch <= 122 || ch >= 65 && ch <= 90)
+			{
+				status = 12;
+				str_value.push_back(ch);
+				break;
+			}
+		case 3:
+			if (ch >= 48 && ch <= 57)
+			{
+				status = 3;
+				str_value.push_back(ch);
+				break;
+			}
+			if (ch == 46)
+			{
+				status = 6;
+				str_value.push_back(ch);
+				break;
+			}
+			if (ch == 41)
+			{
+				status = 4;
+				output.push_back(Term(Term::to_value(str_value)));
+				str_value.clear();
+				output.push_back(Term(ch));
+				break;
+			}
+			if (ch == 43 || ch == 42 || ch == 45 || ch == 47)
+			{
+				status = 2;
+				output.push_back(Term(Term::to_value(str_value)));
+				output.push_back(Term(ch));
+				str_value.clear();
+				break;
+			}
+		case 4:
+			if (ch == 41)
+			{
+				status = 4;
+				output.push_back(Term(ch));
+				break;
+			}
+			if (ch == 43 || ch == 42 || ch == 45 || ch == 47)
+			{
+				status = 2;
+				output.push_back(Term(ch));
+				break;
+			}
+		case 6:
+			if (ch >= 48 && ch <= 57)
+			{
+				status = 6;
+				str_value.push_back(ch);
+				break;
+			}
+			if (ch == 41)
+			{
+				status = 4;
+				output.push_back(Term(Term::to_value(str_value)));
+				str_value.clear();
+				output.push_back(Term(ch));
+				break;
+			}
+			if (ch == 43 || ch == 42 || ch == 45 || ch == 47)
+			{
+				status = 2;
+				output.push_back(Term(Term::to_value(str_value)));
+				str_value.clear();
+				output.push_back(Term(ch));
+				break;
+			}
+		case 7:
+			if (ch == 40)
+			{
+				status = 7;
+				output.push_back(Term(ch));
+				break;
+			}
+			if (ch >= 48 && ch <= 57)
+			{
+				status = 1;
+				str_value.push_back(ch);
+				break;
+			}
+			if (ch >= 97 && ch <= 122 || ch >= 65 && ch <= 90)
+			{
+				status = 10;
+				str_value.push_back(ch);
+				break;
+			}
+		case 8:
+			if (ch >= 97 && ch <= 122 || ch >= 65 && ch <= 90)
+			{
+				status = 8;
+				str_value.push_back(ch);
+				break;
+			}
+			if (ch == 61)
+			{
+				output.push_back(Term(str_value));
+				str_value.clear();
+				output.push_back(Term(ch));
+				status = 9;
+				break;
+			}
+			if (ch == 43 || ch == 42 || ch == 45 || ch == 47)
+			{
+				output.push_back(Term(str_value));
+				str_value.clear();
+				output.push_back(Term(ch));
+				status = 11;
+				break;
+			}
+		case 9:
+			if (ch == 40)
+			{
+				status = 9;
+				output.push_back(Term(ch));
+				break;
+			}
+			if (ch >= 97 && ch <= 122 || ch >= 65 && ch <= 90)
+			{
+				status = 10;
+				str_value.push_back(ch);
+				break;
+			}
+			if (ch >= 48 && ch <= 57)
+			{
+				status = 1;
+				str_value.push_back(ch);
+				break;
+			}
+		case 10:
+			if (ch >= 97 && ch <= 122 || ch >= 65 && ch <= 90)
+			{
+				status = 10;
+				str_value.push_back(ch);
+				break;
+			}
+			if (ch == 43 || ch == 42 || ch == 45 || ch == 47)
+			{
+				output.push_back(Term(str_value));
+				str_value.clear();
+				output.push_back(Term(ch));
+				status = 11;
+				break;
+			}
+		case 11:
+			if (ch == 40)
+			{
+				status = 9;
+				output.push_back(Term(ch));
+				break;
+			}
+			if (ch >= 97 && ch <= 122 || ch >= 65 && ch <= 90)
+			{
+				status = 12;
+				str_value.push_back(ch);
+				break;
+			}
+			if (ch >= 48 && ch <= 57)
+			{
+				status = 3;
+				str_value.push_back(ch);
+				break;
+			}
+		case 12:
+			if (ch >= 97 && ch <= 122 || ch >= 65 && ch <= 90)
+			{
+				status = 12;
+				str_value.push_back(ch);
+				break;
+			}
+			if (ch == 43 || ch == 42 || ch == 45 || ch == 47)
+			{
+				status = 2;
+				output.push_back(Term(str_value));
+				str_value.clear();
+				output.push_back(Term(ch));
+				break;
+			}
+			if (ch == 41)
+			{
+				status = 4;
+				output.push_back(Term(str_value));
+				str_value.clear();
+				output.push_back(Term(ch));
+				break;
+			}
+		}
+	}
+	if (str_value.empty() != true)
+	{
+		if (str.back() >= 48 && str.back() <= 57)
+		{
+			output.push_back(Term(Term::to_value(str_value)));
+			str_value.clear();
+		}
+		if (str.back() >= 97 && str.back() <= 122 || str.back() >= 65 && str.back() <= 90)
+		{
+			output.push_back(Term(str_value));
+			str_value.clear();
+		}
+		
+	}
+	return output;
+}
+void Parser::del_ch(std::string& input, Stack<std::vector, int>& S, int& status,int& parenthesis_counter)
+{
+	if (input.back() == 40)
+		parenthesis_counter--;
+	if (input.back() == 41)
+		parenthesis_counter++;
+	Interface::delete_ch();
+	S.pop();
+	input.pop_back();
+	status = S.top();
+}
+int Parser::add_ch(std::string& input, Stack<std::vector, int>& S,int st,char& ch)
+{
+	S.push(st);
+	Interface::print_ch(ch);
+	input.push_back(ch);
+	return st;
+}
+void Parser::synt_analis_fsm(int& parenthesis_counter,std::string& input, Stack<std::vector, int>& S,int status)	
+{
+	char ch = _getch();
+	S.push(0);
+	while (ch != 13 || parenthesis_counter != 0 || status == 2 || status == 10 ||status==11 || status ==13 || status ==5 )
+	{
+		switch (status)
+		{
+		case 0:
+			if (ch >= 97 && ch <= 122 || ch >= 65 && ch <= 90)
+			{
+				status = add_ch(input, S, 8, ch);
+				break;
+			}
+			if ((ch >= 48 && ch <= 57))
+			{
+				status = add_ch(input, S, 1, ch);
 				break;
 			}
 
 			if (ch == 40)
 			{
-				input.push_back(ch);
-				status = 0;
+				status = add_ch(input, S, 7, ch);
 				parenthesis_counter++;
-				Interface::print_ch(ch);
 				break;
 			}
 			if (ch == 8)
 			{
 				if (input.size() == 0) break;
-				if (input.back() == 40)
-					parenthesis_counter--;
-				if (input.back() == 41)
-					parenthesis_counter++;
 				if (S.size() == 1 && S.top() == 0)
 				{
 					Interface::delete_ch();
 					input.pop_back();
 					break;
 				}
-				Interface::delete_ch();
-				S.pop();
-				input.pop_back();
-				status = S.top();
+				del_ch(input, S, status, parenthesis_counter);
 				break;
 			}
+			
 			else
 			{
 				Interface::print_red_ch(ch);
@@ -65,24 +380,28 @@ std::string Parser::term_analis()
 		case 1:
 			if (ch == 43 || ch == 42 || ch == 45 || ch == 47)
 			{
-				status = 2;
-				Interface::print_ch(ch);
-				input.push_back(ch);
-				S.push(2);
+				status = add_ch(input,S,2,ch);
 				break;
 			}
 			if (ch == 8)
 			{
-				if (input.back() == 41)
-					parenthesis_counter++;
-				if (input.back() == 40)
-					parenthesis_counter--;
-				Interface::delete_ch();
-				S.pop();
-				input.pop_back();
-				status = S.top();
+				del_ch(input, S, status, parenthesis_counter);
 				break;
 			}
+			if (ch == 46)
+			{
+				status = add_ch(input, S, 6, ch);
+				break;
+			}
+			if (ch >= 48 && ch <= 57)
+			{
+				status = 1;
+				input.push_back(ch);
+				Interface::print_ch(ch);
+				break;
+			}
+		
+			
 			else
 			{
 				Interface::print_red_ch(ch);
@@ -92,31 +411,28 @@ std::string Parser::term_analis()
 		case 2:
 			if (ch == 40)
 			{
-				status = 0;
+				status = add_ch(input, S, 0, ch);
 				parenthesis_counter++;
-				Interface::print_ch(ch);
-				input.push_back(ch);
-				S.push(0);
 				break;
 			}
 			if ((ch >= 48 && ch <= 57))
 			{
-				status = 3;
-				Interface::print_ch(ch);
-				input.push_back(ch);
-				S.push(3);
+				status = add_ch(input, S, 3, ch);
 				break;
 			}
 			if (ch == 8)
 			{
-				if (input.back() == 40)
-					parenthesis_counter--;
-				if (input.back() == 41)
-					parenthesis_counter++;
-				Interface::delete_ch();
-				S.pop();
-				input.pop_back();
-				status = S.top();
+				del_ch(input,S,status,parenthesis_counter);
+				break;
+			}
+			if (ch >= 97 && ch <= 122 || ch >= 65 && ch <= 90)
+			{
+				status = add_ch(input, S, 12, ch);
+				break;
+			}
+			if (ch == 13)
+			{
+				status = 13;
 				break;
 			}
 			else
@@ -128,7 +444,6 @@ std::string Parser::term_analis()
 		case 3:
 			if (ch == 41)
 			{
-				status = 4;
 				parenthesis_counter--;
 				if (parenthesis_counter < 0)
 				{
@@ -136,31 +451,38 @@ std::string Parser::term_analis()
 					status = 5;
 					break;
 				}
-				Interface::print_ch(ch);
-				input.push_back(ch);
-				S.push(4);
+				status = add_ch(input, S, 4, ch);
 				break;
 			}
 			if (ch == 43 || ch == 42 || ch == 45 || ch == 47)
 			{
-				status = 2;
-				Interface::print_ch(ch);
-				S.push(2);
-				input.push_back(ch);
+				status = add_ch(input, S, 2, ch);
 				break;
 			}
 			if (ch == 8)
 			{
-				if (input.back() == 40)
-					parenthesis_counter--;
-				if (input.back() == 41)
-					parenthesis_counter++;
-				Interface::delete_ch();
-				S.pop();
-				input.pop_back();
-				status = S.top();
+				del_ch(input, S, status, parenthesis_counter);
 				break;
 			}
+			if (ch == 46)
+			{
+				status = add_ch(input, S, 6, ch);
+				break;
+			}
+
+			if (ch == 13)
+			{
+				status = 13;
+				break;
+			}
+			if (ch >= 48 && ch <= 57)
+			{
+				status = 3;
+				input.push_back(ch);
+				Interface::print_ch(ch);
+				break;
+			}
+			
 			else
 			{
 				Interface::print_red_ch(ch);
@@ -175,6 +497,7 @@ std::string Parser::term_analis()
 				if (parenthesis_counter < 0)
 				{
 					Interface::print_red_ch(ch);
+					parenthesis_counter++;
 					status = 5;
 					break;
 				}
@@ -184,24 +507,15 @@ std::string Parser::term_analis()
 			}
 			if (ch == 43 || ch == 42 || ch == 45 || ch == 47)
 			{
-				status = 2;
-				Interface::print_ch(ch);
-				input.push_back(ch);
-				S.push(2);
+				status = add_ch(input, S, 2, ch);
 				break;
 			}
 			if (ch == 8)
 			{
-				if (input.back() == 40)
-					parenthesis_counter--;
-				if (input.back() == 41)
-					parenthesis_counter++;
-				Interface::delete_ch();
-				S.pop();
-				input.pop_back();
-				status = S.top();
+				del_ch(input, S, status, parenthesis_counter);
 				break;
 			}
+			
 			else
 			{
 				Interface::print_red_ch(ch);
@@ -215,14 +529,279 @@ std::string Parser::term_analis()
 				Interface::delete_ch();
 				break;
 			}
+			status = 5;
+			break;
+		case 6:
+			if (ch >= 48 && ch <= 57)
+			{
+				status = 6;
+				Interface::print_ch(ch);
+				input.push_back(ch);
+				break;
+			}
+			if (ch == 41)
+			{
+				parenthesis_counter--;
+				if (parenthesis_counter < 0)
+				{
+					Interface::print_red_ch(ch);
+					status = 5;
+					break;
+				}
+				status = add_ch(input, S, 4, ch);
+				break;
+			}
+			if (ch == 43 || ch == 42 || ch == 45 || ch == 47)
+			{
+				status = add_ch(input, S, 2, ch);
+				break;
+			}
+			if (ch == 8)
+			{
+				del_ch(input, S, status, parenthesis_counter);
+				break;
+			}
+			
+			else
+			{
+				Interface::print_red_ch(ch);
+				status = 5;
+				break;
+			}
+		case 7:
+			if (ch == 40)
+			{
+				input.push_back(ch);
+				status = 7;
+				parenthesis_counter++;
+				Interface::print_ch(ch);
+				break;
+			}
+			if (ch >= 48 && ch <= 57)
+			{
+				status = add_ch(input, S, 1, ch);
+				break;
+			}
+			if (ch >= 97 && ch <= 122 || ch >= 65 && ch <= 90)
+			{
+				status = add_ch(input, S, 10, ch);
+				break;
+			}
+			if (ch == 8)
+			{
+				del_ch(input, S, status, parenthesis_counter);
+				break;
+			}
+			
+			else
+			{
+				Interface::print_red_ch(ch);
+				status = 5;
+				break;
+			}
+		case 8:
+			if (ch >= 97 && ch <= 122 || ch >= 65 && ch <= 90)
+			{
+				status = 8;
+				input.push_back(ch);
+				Interface::print_ch(ch);
+				break;
+			}
+			if (ch == 61)
+			{
+				status = add_ch(input, S, 9, ch);
+				break;
+			}
+			if (ch == 43 || ch == 42 || ch == 45 || ch == 47)
+			{
+				status = add_ch(input, S, 11, ch);
+				break;
+			}
+			if (ch == 8)
+			{
+				status = S.top();
+				input.pop_back();
+				Interface::delete_ch();
+				break;
+			}
+			
+			else
+			{
+				Interface::print_red_ch(ch);
+				status = 5;
+				break;
+			}
+		case 9:
+			if (ch == 40)
+			{
+				Interface::print_ch(ch);
+				input.push_back(ch);
+				parenthesis_counter++;
+				break;
+			}
+			if (ch >= 97 && ch <= 122 || ch >= 65 && ch <= 90)
+			{
+				status = add_ch(input, S, 10, ch);
+				break;
+			}
+			if (ch >= 48 && ch <= 57)
+			{
+				status = add_ch(input,S,1,ch);
+				break;
+			}
+			if (ch == 8)
+			{
+				del_ch(input, S, status, parenthesis_counter);
+				break;
+			}
+			
+			else
+			{
+				Interface::print_red_ch(ch);
+				status = 5;
+				break;
+			}
+		case 10:
+			if (ch >= 97 && ch <= 122 || ch >= 65 && ch <= 90)
+			{
+				status = 10;
+				Interface::print_ch(ch);
+				input.push_back(ch);
+				break;
+			}
+			if (ch == 43 || ch == 42 || ch == 45 || ch == 47)
+			{
+				status = add_ch(input, S, 11, ch);
+				break;
+			}
+			if (ch == 8)
+			{
+				del_ch(input, S, status, parenthesis_counter);
+				break;
+			}
+			if (ch == 13)
+			{
+				status = 13;
+				break;
+			}
+			else
+			{
+				Interface::print_red_ch(ch);
+				status = 5;
+				break;
+			}
+		case 11:
+			if (ch == 40)
+			{
+				status = add_ch(input, S, 9, ch);
+				parenthesis_counter++;
+				break;
+			}
+			if (ch >= 97 && ch <= 122 || ch >= 65 && ch <= 90)
+			{
+				status = add_ch(input, S, 12, ch);
+				break;
+			}
+			if (ch >= 48 && ch <= 57)
+			{
+				status = add_ch(input, S, 3, ch);
+				break;
+			}
+			if (ch == 8)
+			{
+				del_ch(input, S, status, parenthesis_counter);
+				break;
+			}
+			if (ch == 13)
+			{
+				status = 13;
+				break;
+			}
+			else
+			{
+				Interface::print_red_ch(ch);
+				status = 5;
+				break;
+			}
+		case 12:
+			if (ch >= 97 && ch <= 122 || ch >= 65 && ch <= 90)
+			{
+				status = 12;
+				Interface::print_ch(ch);
+				input.push_back(ch);
+				break;
+			}
+			if (ch == 41)
+			{
+				parenthesis_counter--;
+				if (parenthesis_counter < 0)
+				{
+					Interface::print_red_ch(ch);
+					status = 5;
+					break;
+				}
+				status = add_ch(input, S, 4, ch);
+				break;
+			}
+			if (ch == 43 || ch == 42 || ch == 45 || ch == 47)
+			{
+				status = add_ch(input, S, 2, ch);
+				break;
+			}
+			if (ch == 8)
+			{
+				del_ch(input, S, status, parenthesis_counter);
+				break;
+			}
+			if (ch == 13)
+			{
+				status = 13;
+				break;
+			}
+			else
+			{
+				Interface::print_red_ch(ch);
+				status = 5;
+				break;
+			}
+		case 13:
+			if (ch != 13)
+			{
+				status = S.top();
+				break;
+			}
+			Interface::print_red_ch(63);
+			status = 5;
+			break;
 		}
 		ch = _getch();
 	}
-	std::cout << std::endl;
-	if (parenthesis_counter != 0)
+}
+std::string Parser::synt_analis()
+{
+	int parenthesis_counter;
+	std::string input;
+	do
 	{
+		parenthesis_counter = 0;
+		input.clear();
+		Stack<std::vector, int> S(std::vector<int> {});
+		int status = 0;
+		synt_analis_fsm(parenthesis_counter, input, S, status);
+		
 		std::cout << std::endl;
-		std::cout << "Check the number of parenthesis";
-	}
+		if (parenthesis_counter != 0)
+		{
+			std::cout << "Check the number of parentthesis" << std::endl;
+			std::cout << "Click ENTER to rewrite expression";
+			while (_getch() != 13)
+			{
+				continue;
+			}
+			Interface::clear_console();
+		}
+	} while (parenthesis_counter != 0);
+	
 	return input;
 }
+
--- include/parser/parser.h
+++ include/parser/parser.h
@@ -4,11 +4,45 @@
 #include <conio.h>
 #include "stack.h"
 #include <vector>
+#include <string>
 
+
+class Term
+{
+public:
+	enum class Type
+	{
+		NUMBER,
+		CONSTANT,
+		OPERATOR,
+		OPEN_BRACK,
+		CLOSE_BRACK,
+		VALUE,
+		FUNK,
+		EQ
+	};
+private:
+	Type type;
+	std::string value_str;
+public:
+	Term(double value);
+	Term(char ch);
+	Term(std::string str);
+	
+	static double to_value(std::string str);
+	std::string get_value();
+	Type get_type();
+	
+
+};
 class Parser
 {	
 public:
+	static void del_ch(std::string& input, Stack<std::vector, int>& S,int& status, int& parenthesis_counter);
+	static int add_ch(std::string& input, Stack<std::vector, int>& S, int status, char& ch);
 	static std::vector<char> pars(std::string str);
-	static std::string term_analis();
-	
+	static std::string synt_analis();
+	static std::vector<Term> term_analis(std::string str);
+	static void synt_analis_fsm(int& parenthesis_counter, std::string& input, Stack<std::vector, int>& S, int status);
+	static void value_analis_fsm(char ch,std::string input, Stack<std::vector, int> S);
 };
